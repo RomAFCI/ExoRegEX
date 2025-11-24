b3fb5c973b596860aca0d0cd6ffcb076028fd1c0
RomAFCI <adnot.rom@gmail.com>
2025-11-24 13:28:45 +0100

    fin de l'exo regex validée

--- js.js
+++ js.js
@@ -1,4 +1,43 @@
-// const = querySelector
+const input = document.querySelector(".input");
+
+const info = document.querySelector(".info");
+
+const regexMin = /[a-z]/;
+const regexMaj = /[A-Z]/;
+const regexChiffre = /[0-9]/;
+const regexSpecial = /[^A-Za-z0-9]/;
+
+function chekMDP(mdp) {
+  if (mdp.length < 8) return false;
+  if (!mdp.match(regexMin)) return false;
+  if (!mdp.match(regexMaj)) return false;
+  if (!mdp.match(regexChiffre)) return false;
+  if (!mdp.match(regexSpecial)) return false;
+  return true;
+}
+
+input.addEventListener("input", () => {
+    if (chekMDP(input.value)) {
+        info.textContent = "Mot de passe sécurisé";
+        info.style.color = "green";
+    } else {
+        info.textContent = "Mot de passe faible";
+        info.style.color = "red";
+    }
+});
+
+// LISTE MDP VALIDE :
+
+// Azerty1!,
+// Bonjour2@,
+// Test123#,
+// P@ssword9,
+// HelloWorld3!,
+// JsDev2024*,
+// ChocoCake7%,
+// DragonFire8$,
+// WebMaster4&,
+// ValidMdp5?
 
 
-// addEventListener
+console.log("Connect to JS");
